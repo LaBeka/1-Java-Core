e18d85e01b86543a38f37de9477480e9a31c9005
Begayym <begayymm@gmail.com>
2025-09-08 09:49:24 +0200

    first commit

---
+++ src/main/java/org/example/Chat.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Chat {
+ private String name;
+ private String message;
+ private int length;
+
+  public Chat(String name, String message, int length) {
+    this.name = name;
+    this.message = message;
+    this.length = length;
+  }
+
+  @Override
+  public String toString() {
+    return "Chat{" +
+        "name='" + name + '\'' +
+        ", message='" + message + '\'' +
+        ", length=" + length +
+        '}';
+  }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Chat chat = new Chat("Lolly", "hello", 5);
+    System.out.println("Hello world! 1 " + chat.toString());
+  }
+}
