4557977bb2b02c431698aaddb398ced09f02ef32
Begayym <begayymm@gmail.com>
2025-09-08 09:54:36 +0200

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
   public static void main(String[] args) {
 
+    System.out.printf("");
+    System.out.printf("");
+    System.out.printf("");
+    System.out.println();
+    System.out.printf("");
     Chat chat = new Chat("Lolly", "hello", 5);
     System.out.println("Hello world! 1 " + chat.toString());
   }
